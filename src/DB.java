da2bca630684557318137648c48779665633f148
Shubham Verma <64227183+Quantumo0o@users.noreply.github.com>
2023-10-11 11:05:22 +0530

    DB.java

--- src/DB.java
+++ src/DB.java
@@ -2,21 +2,35 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DB {
-	public static Connection getConnection() {
-		Connection con = null;
+    public static Connection getConnection() {
+        Connection con = null;
 
-		String xuri = "amRiYzpteXNxbDovL3VjZnI3cWh2NG56ZmM0dWE6NmZhMUxZSnE1ZXFLYkhPODRVcmtAYnhmYm11a2JicmJzcDUwNGZicXQtbXlzcWwuc2VydmljZXMuY2xldmVyLWNsb3VkLmNvbTozMzA2L2J4ZmJtdWtiYnJic3A1MDRmYnF0";
-		String xuri1 = "dWNmcjdxaHY0bnpmYzR1YQ==";
-		String xuri2 = "NmZhMUxZSnE1ZXFLYkhPODRVcms=";
+        // Database connection details (In base64 encoded for security)
+        String xuri = "jdbc:mysql://localhost:3306/mydatabase";
+	String xuri1 = "myUsername";
+	String xuri2 = "myPassword";
 
-		try {
 
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			con = DriverManager.getConnection(password.Youo(xuri), password.Youo(xuri1), password.Youo(xuri2));
+        try {
+            // Load the MySQL JDBC driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-		return con;
-	}
+            // Decode and use connection details
+            String dbUrl = password.Youo(xuri);
+            String username = password.Youo(xuri1);
+            String userPassword = password.Youo(xuri2);
+
+            if (dbUrl.isEmpty() || username.isEmpty() || userPassword.isEmpty()) {
+                // Prompt the user to fill in their information
+                System.out.println("Please provide your database connection details in 'xuri', 'xuri1', and 'xuri2'.");
+            } else {
+                // Attempt to establish a database connection
+                con = DriverManager.getConnection(dbUrl, username, userPassword);
+            }
+        } catch (Exception e) {
+            // Handle exceptions (e.g., log the error or show a user-friendly error message)
+            System.out.println("An error occurred while connecting to the database: " + e.getMessage());
+        }
+        return con;
+    }
 }
